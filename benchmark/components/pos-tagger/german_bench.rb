8c51036c9399bbbc9898e37454eb098e943e3247
Nila <nilakrachtin@olery.com>
2014-06-20 18:25:58 +0200

    set up benchmark languages

---
+++ benchmark/components/pos-tagger/german_bench.rb
@@ -0,0 +1,36 @@
+require 'benchmark_helper'
+
+OpenerBenchmark.benchmark 'pos-tagger' do
+  set :version, Opener::POSTagger::VERSION
+  set :language, 'german'
+
+  setup do
+    identifier  = Opener::LanguageIdentifier.new(:kaf => true)
+    tokenizer   = Opener::Tokenizer.new(:kaf => true)
+    @pos_tagger = Opener::POSTagger.new
+
+    @small_review = tokenizer.run(
+      identifier.run(read_fixture('german/small.txt'))
+    )
+
+    @medium_review = tokeniozer.run(
+      identifier.run(read_fixture('german/medium.txt'))
+    )
+
+    @large_review = tokenizer.run(
+      identifier.run(read_fixture('german/large.txt'))
+    )
+  end
+
+  bench 'small review', :words => 30 do
+    @pos_tagger.run(@small_review)
+  end
+
+  bench 'medium review', :words => 50 do
+    @pos_tagger.run(@medium_review)
+  end
+
+  bench 'large review', :words => 130 do
+    @pos_tagger.run(@large_review)
+  end
+end
