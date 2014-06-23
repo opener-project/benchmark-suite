fa1420f42a702df961fdef89fdd1132c35ebe2d1
Yorick Peterse <yorickpeterse@olery.com>
2014-06-23 17:55:00 +0200

    Added preparation steps for the opinion detector.

--- benchmark/components/opinion_detector_basic_bench.rb
+++ benchmark/components/opinion_detector_basic_bench.rb
@@ -4,10 +4,20 @@ OpenerBenchmark.benchmark_languages 'opinion-detector-basic' do
   set :version, Opener::OpinionDetectorBasic::VERSION
 
   setup do
+    require_env_vars!(%w{POLARITY_LEXICON_PATH PROPERTY_TAGGER_LEXICONS_PATH})
+
+    steps = [
+      :LanguageIdentifier,
+      :Tokenizer,
+      :POSTagger,
+      :PolarityTagger,
+      :PropertyTagger
+    ]
+
     @component     = Opener::OpinionDetectorBasic.new
-    @small_review  = fixture(:small)
-    @medium_review = fixture(:medium)
-    @large_review  = fixture(:large)
+    @small_review  = prepare_kaf(:small, steps)
+    @medium_review = prepare_kaf(:medium, steps)
+    @large_review  = prepare_kaf(:large, steps)
   end
 
   include_shared_benchmark :word_sizes
