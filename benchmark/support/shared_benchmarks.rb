6781c6ef96d95e5670218390fce8157bd3391aa1
Yorick Peterse <yorickpeterse@olery.com>
2014-06-24 13:22:29 +0200

    Updated the shared benchmarks.

--- benchmark/support/shared_benchmarks.rb
+++ benchmark/support/shared_benchmarks.rb
@@ -1,15 +1,15 @@
 # This shared benchmark requires the instance variable `@component` to be set
 # to an object to benchmark.
 OpenerBenchmark.shared_benchmark :word_sizes do
-  bench 'small review', :words => 30 do
+  bench 'small review' do
     @component.run(@small_review)
   end
 
-  bench 'medium review', :words => 50 do
+  bench 'medium review' do
     @component.run(@medium_review)
   end
 
-  bench 'large review', :words => 130 do
+  bench 'large review' do
     @component.run(@large_review)
   end
 end
